141560fa32e5bfa4ceeced4fcbc057a0bc0d058a
kambata940 <kamen@tradeo.com>
2016-03-07 22:53:01 +0200

    Minor style changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,17 +5,17 @@ class MessagesController < ApplicationController
   end
 
   def create
-    username = session[:username]
-    current_chat_room = ChatRoom.find(params[:id])
-    @message = Message.new(user_name: username,
+    @message = Message.new(user_name: session[:username],
                            content: params[:content],
-                           chat_room: current_chat_room)
+                           chat_room: chat_room)
+
     respond_to do |format|
       if @message.save
-        format.js {render 'create'}
+        format.js { render 'create' }
       else
-        format.js {render nothing: true}
+        format.js { render nothing: true }
       end
+
       format.html { redirect_to current_chat_room_path }
     end
   end
