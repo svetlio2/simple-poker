a5b5c1fb7ca17659b5b92d1ce6bf943dcc231a1a
kambata940 <kamen@tradeo.com>
2016-04-10 19:26:46 +0300

    Make User's name uniq

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,25 @@
 class User
   attr_reader :name
+  @ids = []
+
+  class << self
+    attr_accessor :ids
+  end
+
   def initialize(name:)
     @name = name.strip
   end
 
   def save
-    is_valid?
+    return unless is_valid?
+
+    User.ids << @name
+    true
   end
 
   private
-  
+
   def is_valid?
-    !name.empty? && name.size < 40
+    !name.empty? && name.size < 40 && User.ids.exclude?(@name)
   end
 end
